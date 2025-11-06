11601f4948b015868cda2710763a8e05971ef305
liuranran <864749024@qq.com>
2025-11-06 18:03:00 +0800

    fixed:样式添加

--- src/views/goods/goods-manage/Create.vue
+++ src/views/goods/goods-manage/Create.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="detail-page-wrapper">详情{{ a }}</div>
+  <div class="page-main">
+    <el-tabs v-model="activeName" type="card" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="User" name="first"></el-tab-pane>
+      <el-tab-pane label="Config" name="second"></el-tab-pane>
+      <el-tab-pane label="Role" name="third"></el-tab-pane>
+      <el-tab-pane label="Task" name="fourth"></el-tab-pane>
+    </el-tabs>
+    <div class="section-wrapper">
+      <div class="c-section-header">基础信息</div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-const a = ref(0)
+
+import type { TabsPaneContext } from 'element-plus'
+
+const activeName = ref('first')
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
 </script>
 
 <style lang="less" scoped></style>
